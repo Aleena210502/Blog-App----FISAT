cb8b24b0a074391496822d617b52005956d0875a
Aleena Joshy <joshy200467@gmail.com>
2025-09-02 20:04:05 +0530

    Viewall completed

--- app.js
+++ app.js
@@ -42,6 +42,34 @@ app.post("/create",async(req,res)=>{
 
 })
 
+// View All
+
+app.post("/viewall",(req,res)=>{
+
+    let token=req.headers.token
+
+    jwt.verify(token,"blogApp",(error,decoded)=>{
+        if (decoded && decoded.email) {
+
+            postModel.find().then(
+                (items)=>{
+                    res.json(items)
+                }
+            ).catch(
+                (error)=>{
+                    res.json({"status":"error"})
+
+                }
+            )
+            
+        } else {
+            res.json({"status":"Invalid Authentication"})
+            
+        }
+    })
+})
+
+
 // Sign In
 app.post("/signIn",async(req,res)=>{
 
